2638be66ba879b9970753a54ec3e2030e1d36cc4
Zu18 <zuhra.galieva.2014@gmail.com>
2022-08-25 09:40:16 +0100

    Fixed typo

--- def number_to_string(num).rb
+++ def number_to_string(num).rb
@@ -2,7 +2,6 @@
 Convert a Number to a String!
 
 We need a function that can transform a number (integer) into a string.
-What ways of achieving this do you know?
 Examples (input --> output):
 123  --> "123"
 999  --> "999"
@@ -13,11 +12,10 @@ Examples (input --> output):
 # Creating the method tham will convert a Number to a String.
 
 def number_to_string(num)
-  num.to_s                       
+  num.to_s
 end
 
 
 # Calling the method for testing (using "p" method to view debug output):
 
 p number_to_string(123)
-
